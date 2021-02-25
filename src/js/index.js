d8edfabe9f3c9b83e928c07e0a5e56fc67bc0dc6
dimonabinsk <dimonabinsk@gmail.com>
2021-02-25 23:54:16 +0300

    делаем адаптивку основно страницы

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import WOW from "wow.js";
 import $ from "jquery";
 import "slick-carousel";
-import { createFocusTrap } from "focus-trap";
+// import { createFocusTrap } from "focus-trap";
 
 let wow = new WOW({
   boxClass: "animation",
@@ -22,54 +22,54 @@ wow.init();
 // }
 
 function throttle(func, time) {
-    let isThrottled = false;
-    return function () {
-      if (isThrottled) return;
-      let ctx = this;
-      let args = arguments;
-      func.apply(ctx, args);
-      isThrottled = true;
-      setTimeout(() => {
-        isThrottled = false;
-      }, time);
-    };
-  }
+  let isThrottled = false;
+  return function () {
+    if (isThrottled) return ;
+    let ctx = this;
+    let args = arguments;
+    func.apply(ctx, args);
+    isThrottled = true;
+    setTimeout(() => {
+      isThrottled = false;
+    }, time);
+  };
+}
 
-  window.addEventListener("scroll", throttle(stickyHeader, 300));
+window.addEventListener("scroll", throttle(stickyHeader, 300));
 
-
-
-
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#"]').click(function () {
-  $page.animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 400);
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    400
+  );
   return false;
 });
 
 // Выпадающий список
 
-$(function() {
-	$(".selectlink-control").click(function(){
-		var $menu_popup = $(this).next();
-		$menu_popup.slideToggle(200, function(){
-			$('.selectlink ul').not($menu_popup).slideUp(200);
-			if ($menu_popup.is(':hidden')) {
-				$('body').removeClass('body_pointer');
-			} else {
-				$('body').addClass('body_pointer');
-			}					
-		});  
-		return false;
-	});			
- 
-	$(document).on('click', function(e){
-		if (!$(e.target).closest('.selectlink').length){
-			$('body').removeClass('body_pointer');
-			$('.selectlink ul').slideUp(200);
-		}
-	});
+$(function () {
+  $(".selectlink-control").click(function () {
+    var $menu_popup = $(this).next();
+    $menu_popup.slideToggle(200, function () {
+      $(".selectlink ul").not($menu_popup).slideUp(200);
+      if ($menu_popup.is(":hidden")) {
+        $("body").removeClass("body_pointer");
+      } else {
+        $("body").addClass("body_pointer");
+      }
+    });
+    return false;
+  });
+
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest(".selectlink").length) {
+      $("body").removeClass("body_pointer");
+      $(".selectlink ul").slideUp(200);
+    }
+  });
 });
 
 // Гамбургер
@@ -105,9 +105,6 @@ headerNav.addEventListener("click", () => {
     mobileMenuClose();
   }
 });
-
-
-
 
 window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
